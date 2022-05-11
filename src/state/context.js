bf765e7f66f3aaf2f3a1dbdba3f86080b27263d1
Victory <vickywane@gmail.com>
2022-05-11 21:56:19 +0100

    feat: created partially built DApp

--- src/state/context.js
+++ src/state/context.js
@@ -1,8 +1,4 @@
 import React, {useEffect, useReducer} from "react";
-import Web3 from "web3";
-import {ethers, providers} from "ethers";
-
-const {abi} = require('../contracts/adoptionAbi.json')
 
 export const initialState = {
     isModalOpen: false,
@@ -16,8 +12,6 @@ export const initialState = {
     retrieveAdopters: (id) => {
     },
 };
-
-const {ethereum, web3} = window
 
 const AppContext = React.createContext(initialState);
 export default AppContext;
@@ -41,11 +35,7 @@ const reducer = (state, action) => {
 
 const createEthContractInstance = () => {
     try {
-        const provider = new providers.Web3Provider(ethereum)
-        const signer = provider.getSigner()
-        const contractAddress = process.env.REACT_APP_ADOPTION_CONTRACT_ADDRESS
 
-        return new ethers.Contract(contractAddress, abi, signer)
     } catch (e) {
         console.log('Unable to create ethereum contract. Error:', e)
     }
@@ -55,34 +45,11 @@ export const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const instantiateWeb3 = async _ => {
-        if (ethereum) {
-            try {
-                // Request account access
-                return await ethereum.request({method: "eth_requestAccounts"})
-            } catch (error) {
-                // User denied account access...
-                console.error("User denied account access")
-            }
-        } else if (web3) {
-            return
-        }
 
-        // dispatch({type: 'INITIATE_WEB3', payload: {instance}})
-        return new Web3(Web3.givenProvider || "ws://localhost:8545")
     }
 
     const adoptPet = async id => {
         try {
-            const instance = createEthContractInstance()
-            const accountData = await instantiateWeb3()
-
-            const adoptPet = await instance.adopt(id, {
-                from: accountData[0]
-            })
-
-            console.log(
-                "ADOPT PET", adoptPet // 0x5FbDB2315678afecb367f032d93F642f64180aa3
-            )
 
             dispatch({
                 type: 'SENT_TOAST', payload: {
@@ -104,50 +71,13 @@ export const AppProvider = ({children}) => {
         }
     }
 
-    // const adoptPet = async id => {
-    //     try {
-    //         const instance = createEthContractInstance()
-    //         const adoptPet = await instance.adopt(id)
-    //
-    //         console.log(
-    //             "ADOPT PET", adoptPet // 0x5FbDB2315678afecb367f032d93F642f64180aa3
-    //         )
-    //
-    //         dispatch({
-    //             type: 'SENT_TOAST', payload: {
-    //                 toastVisibility: true
-    //             }
-    //         })
-    //
-    //         // close success toast after 3s
-    //         setTimeout(() => {
-    //             dispatch({
-    //                 type: 'SENT_TOAST', payload: {
-    //                     toastVisibility: false
-    //                 }
-    //             })
-    //         }, 3000)
-    //
-    //     } catch (e) {
-    //         console.log("ERROR:", e)
-    //     }
-    // }
-
     const retrieveAdopters = async _ => {
         try {
-            const instance = createEthContractInstance()
 
-            return await instance.getAdopters()
         } catch (e) {
             console.log("RETRIEVING:", e)
         }
     }
-
-    useEffect(() => {
-        (async () => {
-            await instantiateWeb3()
-        })()
-    })
 
     return (
         <AppContext.Provider
